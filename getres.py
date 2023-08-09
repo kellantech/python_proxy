9e42fbcb8dc754a7b75262a8559556681cf535d3
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2023-08-09 14:44:31 -0400

    Create getres.py

---
+++ getres.py
@@ -0,0 +1,42 @@
+import requests
+import re
+from urllib.parse import urljoin
+from logs import log
+from bs4 import BeautifulSoup
+def mod_res(r):
+  url = "https://jade-secretive-plastic.glitch.me/"
+  l = []
+  
+  soup = BeautifulSoup(r,"html.parser")
+  for link in soup.findAll("a"):
+    l.append(link.get("href"))
+  for i in l:
+    r = r.replace(i,f"{url}/search?query={i}")  
+  return r
+def change_res(r, abs_url):
+  
+    absolutize = lambda m: ' src="' + urljoin(abs_url, m.group(1)) + '"'
+    htm = re.sub(r' src="([^"]+)"', absolutize, r)
+    absolutize2 = lambda m: ' href="' + urljoin(abs_url, m.group(1)) + '"'
+    html = re.sub(r' href="([^"]+)"', absolutize2, htm)
+    return html
+  
+
+
+
+def get_res(q):
+    
+    r = (requests.get(q).text)
+    
+
+    r2 = r.replace('\n', '').replace('\r', '').replace('\t','')
+    
+
+    x = change_res(r2,q)
+  
+
+    y = mod_res(x)
+
+    t = re.sub("\\'", "'", y)
+    
+    return t
